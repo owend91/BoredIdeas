4e7f2b49161bbb3ca3998cbc8a7d501f2062d122
David Owen <owend91@gmail.com>
2023-03-06 13:01:48 -0700

    Initial Commit

---
+++ BoredIdeas/BoredIdeasApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredIdeasApp.swift
+//  BoredIdeas
+//
+//  Created by David Owen on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BoredIdeasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
